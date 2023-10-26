6458a39f005f422d547ca67c0c1ec32b822de94a
Parallax <kussler@mail.ru>
2023-10-26 10:20:55 +0300

    initial

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,6 +2,7 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from django.utils.text import slugify
 
 
 class Command(BaseCommand):
@@ -14,4 +15,7 @@ class Command(BaseCommand):
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            Phone(id=phone['id'], name=phone['name'], price=phone['price'], image=phone['image'],
+                  release_date=phone['release_date'], lte_exists=phone['lte_exists'],
+                  slug=slugify(phone['name'])).save()
+            print(f'Телефон {phone["name"]} добавлен')
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +9,20 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get('sort')
+    phones = list(Phone.objects.all())
+    if sort == 'name':
+        phones = sorted(phones, key=lambda x: x.name)
+    elif sort == 'min_price':
+        phones = sorted(phones, key=lambda x: x.price)
+    elif sort == 'max_price':
+        phones = sorted(phones, key=lambda x: x.price, reverse=True)
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = list(Phone.objects.filter(slug=slug))
+    context = {'phone': phone[0]}
     return render(request, template, context)
