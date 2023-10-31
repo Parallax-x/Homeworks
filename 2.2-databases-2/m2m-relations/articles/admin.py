ae54899a62724acc6155371c7982ef73b4f361c0
Parallax <kussler@mail.ru>
2023-10-31 19:31:08 +0300

    changed formset

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -7,22 +7,28 @@ from .models import Article, Scope, Tag
 
 class RelationshipInlineFormset(BaseInlineFormSet):
     def clean(self):
+        var = 0
         for form in self.forms:
             # В form.cleaned_data будет словарь с данными
             # каждой отдельной формы, которые вы можете проверить
-            main = form.cleaned_data['is_main']
             # вызовом исключения ValidationError можно указать админке о наличие ошибки
             # таким образом объект не будет сохранен,
             # а пользователю выведется соответствующее сообщение об ошибке
-            if not main:
-                raise ValidationError('Укажите основной раздел')
-            break
+            if len(form.cleaned_data) == 0:
+                continue
+            if form.cleaned_data['is_main']:
+                var += 1
+        if var == 0:
+            raise ValidationError('Укажите основной раздел')
+        elif var > 1:
+            raise ValidationError('Основным может быть только один раздел')
         return super().clean()  # вызываем базовый код переопределяемого метода
 
 
 class RelationshipInline(admin.TabularInline):
     model = Scope
     formset = RelationshipInlineFormset
+    extra = 2
 
 
 @admin.register(Article)
