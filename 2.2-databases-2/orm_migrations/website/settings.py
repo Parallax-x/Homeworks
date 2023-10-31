08044004f0efced0fa41760e009d780c39b66834
Parallax <kussler@mail.ru>
2023-10-31 11:21:46 +0300

    changed1

--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'postgres',
-        'PASSWORD': 'Gloriam11',
+        'PASSWORD': os.getenv('pass'),
     }
 }
 
