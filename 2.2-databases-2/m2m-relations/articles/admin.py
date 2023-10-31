8d9e622c1387290dfb19e30afbc7d2f6e1ec62c1
Parallax <kussler@mail.ru>
2023-10-31 15:31:32 +0300

    initial m2m-relations

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            main = form.cleaned_data['is_main']
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+            if not main:
+                raise ValidationError('Укажите основной раздел')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'text', 'published_at', 'image']
+    list_filter = ['published_at']
+    inlines = [RelationshipInline,]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -5,10 +5,9 @@ from articles.models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
+    context = {'object_list': list(Article.objects.all().prefetch_related('tags').order_by('-published_at'))}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
 
     return render(request, template, context)
